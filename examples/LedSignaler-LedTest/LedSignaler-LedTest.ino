b8f29acf558548e992a8091a7469cae3a22eed72
gemi254 <gemi254@192.168.56.1>
2024-12-05 17:52:03 +0200

    Added endtask

--- examples/LedSignaler-LedTest/LedSignaler-LedTest.ino
+++ examples/LedSignaler-LedTest/LedSignaler-LedTest.ino
@@ -1,9 +1,11 @@
 #include <LedSignaler.h>
 
-bool ledState = false;
+// Define the led pin if not defined
 #ifndef LED_BUILTIN
 #define LED_BUILTIN 22
 #endif
+
+bool ledState = false;
 LedSignaler led( LED_BUILTIN, LOW);
 
 #define BUFF_LEN 24
@@ -17,29 +19,41 @@ void serialEvent() {
 		if (inChar == '\n' || inChar == '\r' || cmdBuffPos > BUFF_LEN - 2) {
 			//Mark the array end without \n
 			cmdBuff[cmdBuffPos - 1]='\0';
-      //Serial.printf("cmd: %s\n", cmdBuff);
+      Serial.printf("cmd: %s\n", cmdBuff);
+
       if(strcasecmp(cmdBuff, "on") == 0){
         Serial.printf("on\n");
         led.on();
+
       }else if(strcasecmp(cmdBuff, "off") == 0){
         Serial.printf("ledoff\n");
         led.off();
+
       }else if(strcasecmp(cmdBuff, "enable") == 0){
         Serial.printf("enable\n");
         led.enable();
+
       }else if(strcasecmp(cmdBuff, "disable") == 0){
         Serial.printf("disable\n");
         led.disable();
+
       }else if(strcasecmp(cmdBuff, "pause") == 0){
         Serial.printf("pause\n");
         led.pause();
+
       }else if(strcasecmp(cmdBuff, "resume") == 0){
         Serial.printf("resume\n");
         led.resume();
-      }else if(strncasecmp(cmdBuff, "blink",5)  == 0 ||
-               strncasecmp(cmdBuff, "push",4)   == 0  ||
-               strncasecmp(cmdBuff, "inject",6) == 0) {
-        char cmd[10];
+#if defined(ESP32)
+      }else if(strcasecmp(cmdBuff, "endtask") == 0){
+        Serial.printf("Ending task\n");
+        led.endTask();
+#endif
+      }else if(strncasecmp(cmdBuff, "blink",5)   == 0 ||
+               strncasecmp(cmdBuff, "task",4)    == 0 ||
+               strncasecmp(cmdBuff, "push",4)    == 0 ||
+               strncasecmp(cmdBuff, "inject",6)  == 0) {
+        char cmd[25];
         char *t = strtok (cmdBuff, " ");
         if(t == NULL) break;
         strcpy(cmd, t);
@@ -49,21 +63,32 @@ void serialEvent() {
         int loops = -1;
         t = strtok(NULL, ",");
         if(t != NULL) loops = atoi(t);
+
         if(strcasecmp("blink", cmd) == 0){
           Serial.printf("blink %i, %i\n",type, loops);
           if(loops==-1) led.blink(type);
           else led.blink(type, loops);
+
+#if defined(ESP32)
+        }else if(strcasecmp("task", cmd) == 0){
+          Serial.printf("task blink %i, %i\n",type, loops);
+          if(loops==-1) led.blinkTask(type);
+          else led.blinkTask(type, loops);
+#endif
         }else if(strcasecmp("push", cmd) == 0){
           Serial.printf("push %i, %i\n",type,loops);
           if(loops==-1) led.blinkPush(type);
           else led.blinkPush(type, loops);
+
         }else if(strcasecmp("inject", cmd) == 0){
           Serial.printf("inject %i, %i\n",type,loops);
           if(loops==-1) led.blinkInject(type);
           else led.blinkInject(type, loops);
         }
+
       }else if(strcasecmp(cmdBuff, "status") == 0){
-        Serial.printf("blink: enabled:%i, paused:%i, mode:%i\n",led.isEnabled(), led.isPaused(), led.getMode());
+        Serial.printf("blink: enabled:%i, paused:%i, mode:%i, next: %i ", led.isEnabled(), led.isPaused(), led.getMode(), led.getNextMode());
+
       }else{
         Serial.printf("Unknown cmd\n");
       }
@@ -79,12 +104,33 @@ void setup() {
   Serial.print("\n\n\n\n");
   Serial.flush();
   Serial.printf("Starting ledSignaler v.%s\n", LS_CLASS_VERSION);
+  #if defined(ESP8266)
+  // Start blink task with update on loop
   led.blink(LedSignaler::BTYPE_BLINK1);
+  #else
+  // Start an indepentend thread for handling the led
+  led.blinkTask(LedSignaler::BTYPE_FADE1);
+  #endif
 }
-
+size_t tm = millis();
 void loop() {
   if(Serial.available()) serialEvent();
+
+#if defined(ESP8266)
   led.update();
+#else
+  //On esp32 can use thread (blinkTask) or loop update (blink) mode
+  if(!led.hasTask()) led.update();
+#endif
+
+  // Monitor led mode
+  if(millis() - tm > 3000){
+    Serial.printf("Led mode: %i, next: %i\n", led.getMode(), led.getNextMode());
+#if defined(ESP32)
+    Serial.printf("Led task: %i\n", led.hasTask());
+#endif
+    tm = millis();
+  }
 }
 
 
